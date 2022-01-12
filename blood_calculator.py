dd17f543cce81176a25644004d199da2ad3a9159
William Barbera <william.barbera@duke.edu>
2022-01-12 16:32:35 -0500

    Developed check_HDL and HDL driver for workflow

--- blood_calculator.py
+++ blood_calculator.py
@@ -14,4 +14,15 @@ def accept_input(test_name):
     entry = input("Enter the {} test result: ".format(test_name))
     return int(entry)
 
+
+def check_HDL(HDL_val)
+    if HDL_val >= 60
+        answer = "Normal"
+    elif 60 > HDL >= 40
+        answer = "Borderline Low"
+    else:
+        answer = "Low"
+    return answer
+    
+
 interface()
