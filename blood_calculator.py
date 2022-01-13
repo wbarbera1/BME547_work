70f4a1bca59861c85e357afa2ddaba05f3b708f1
William Barbera <william.barbera@duke.edu>
2022-01-13 16:01:50 -0500

    Added HDL option to interface() - from class

--- blood_calculator.py
+++ blood_calculator.py
@@ -3,9 +3,12 @@ def interface():
     keep_running = True
     while keep_running:
         print("Options:")
+        print("1-HDL")
         print("9-Quit")
         choice = input("Enter your choice: ")
-        if choice == "9":
+        if choice == "1"
+            choice = "HDL"
+        elif choice == "9":
             keep_running = False
     return
 
@@ -13,12 +16,6 @@ def interface():
 def accept_input(test_name):
     entry = input("Enter the {} test result: ".format(test_name))
     return int(entry)
-
-
-def print_result(test_name, test_value, test_class)
-    out_string = "The test value of {} for {} is {}.".format(test_value, test_name, test_class)
-    print(out_string)
-    return
 
 
 def check_HDL(HDL_val)
@@ -29,9 +26,15 @@ def check_HDL(HDL_val)
     else:
         answer = "Low"
     return answer
+
+
+def print_result(test_name, test_value, test_class)
+    out_string = "The test value of {} for {} is {}.".format(test_value, test_name, test_class)
+    print(out_string)
+    return
     
   
-def HDL_driver():
+def HDL_driver(test_selection):
     HDL_val = accept_input("HDL")
     classification = check_HDL(HDL_val)
     print_result("HDL", HDL_value, classification)
