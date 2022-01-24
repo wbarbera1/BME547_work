8c1a75309510228e7b6266995a2131000b62a6e9
William Barbera <william.barbera@duke.edu>
2022-01-24 16:17:35 -0500

    Improved modularity of code

--- dosing.py
+++ dosing.py
@@ -6,7 +6,7 @@
 """
 
 
-def dose_amount():
+def select_diagnosis():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -15,6 +15,9 @@ def dose_amount():
     print("3 - Community-acquired pneumonia")
     print("4 - Pharyngitis/tonsilitis")
     diagnosis = int(input("Enter a number: "))
+    return diagnosis
+
+def patient_weight():
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
@@ -22,16 +25,33 @@ def dose_amount():
     weight_data = weight_input.split(" ")
     weight = float(weight_data[0])
     units = weight_data[1]
+    return weight, units
+
+def conv_weight(weight, units):
     if units == "lb":
         weight = weight / 2.205
+    return weight
+        
+def calc_dosage(weight, diagnosis):
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
+    return dosage_mg_first_day
+    
+def correct_dosage(weight, dosage_mg_first_day):
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
     print("  the correct dosage is {:.1f} mg the first day"
           .format(dosage_mg_first_day))
 
+def dosage_driver():
+    diagnosis = select_diagnosis()
+    weight, units = patient_weight()
+    weight = conv_weight(weight, units)
+    dosage = calc_dosage(weight, diagnosis)
+    correct_dosage(weight, dosage)
+
+
 
 if __name__ == '__main__':
-    dose_amount()
+    dosage_driver()
