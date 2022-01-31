ecf697fbbd155e480062de772210c24ba31b3788
William Barbera <william.barbera@duke.edu>
2022-01-31 16:47:32 -0500

    Fixed syntax errors

--- calc_coord.py
+++ calc_coord.py
@@ -15,10 +15,10 @@ def calc_slope(coord1, coord2):
     slope = (coord2[1] - coord1[1])/(coord2[0] - coord1[0])
     return slope
 
-def y_coord_out(slope, x_val)
+def y_coord_out(slope, x_val):
     y_value = slope*x_val
     return y_value
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     calculate_value()
 
