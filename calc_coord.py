62ce37a16c6ce2fc0445ed5a7af7fae15b983644
William Barbera <william.barbera@duke.edu>
2022-02-04 15:26:01 -0500

    Wrote input functions in calculate_value to input coordinates for line

--- calc_coord.py
+++ calc_coord.py
@@ -1,19 +1,25 @@
 # Calculate Coordinates
 
 def calculate_value():
+    x_coord1 = input("X1: ")
+    y_coord1 = input("Y1: ")
+    x_coord2 = input("X2: ")
+    y_coord2 = input("Y2: ")
+    
     coord1, coord2 = input_coordinates()
     slope = calc_slope(coord1, coord2)
     y_value = y_coord_out(slope, 5)
     print(y_value)
 
-def input_coordinates():
-    coord1 = (0, 0)
-    coord2 = (2, 1)
+def input_coordinates(x_coord1, x_coord2, y_coord1, y_coord2):
+    coord1 = (x_coord1, y_coord1)
+    coord2 = (x_coord2, y_coord2)
     return coord1, coord2
 
 def calc_slope(coord1, coord2):
     slope = (coord2[1] - coord1[1])/(coord2[0] - coord1[0])
     return slope
+
 
 def y_coord_out(slope, x_val):
     y_value = slope*x_val
