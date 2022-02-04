e981b81a37f3ef8c27ad743492bb1f408c9c9b32
William Barbera <william.barbera@duke.edu>
2022-02-04 16:31:08 -0500

    Fixed pycodestyle adjustments.

--- calc_coord.py
+++ calc_coord.py
@@ -12,23 +12,27 @@ def calculate_value():
     y_value = y_coord_out(slope, interc, x_val)
     print("Corresponding y_value = {}".format(y_value))
 
+
 def input_coordinates(x_coord1, x_coord2, y_coord1, y_coord2):
     coord1 = (x_coord1, y_coord1)
     coord2 = (x_coord2, y_coord2)
     return coord1, coord2
 
+
 def calc_slope(coord1, coord2):
     slope = (coord2[1] - coord1[1])/(coord2[0] - coord1[0])
     return slope
+
 
 def calc_intercept(coord1, coord2, slope):
     interc = coord1[1] - slope*coord1[0]
     return interc
 
+
 def y_coord_out(slope, interc, x_val):
     y_value = slope*x_val + interc
     return y_value
 
+
 if __name__ == "__main__":
     calculate_value()
-
--- test_calc_coord.py
+++ test_calc_coord.py
@@ -9,8 +9,6 @@ import pytest
     (-2, -3, 0, -3),
     (-2, -3, 5, -13),
     (-2, -3, -9, 15),
-    
-    ()
     ])
 def test_calc_coord(m, b, x1, expected):
     from calc_coord import y_coord_out
