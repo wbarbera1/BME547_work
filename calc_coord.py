66004e1b76ce5c3c8d207c458be6836e330a3946
William Barbera <william.barbera@duke.edu>
2022-02-04 16:01:33 -0500

    Made inputs into integers and fixed minor errors.

--- calc_coord.py
+++ calc_coord.py
@@ -1,14 +1,14 @@
 # Calculate Coordinates
 
 def calculate_value():
-    x_coord1 = input("X1: ")
-    y_coord1 = input("Y1: ")
-    x_coord2 = input("X2: ")
-    y_coord2 = input("Y2: ")
-    x_val = input("Enter an x-value: )
+    x_coord1 = int(input("X1: "))
+    y_coord1 = int(input("Y1: "))
+    x_coord2 = int(input("X2: "))
+    y_coord2 = int(input("Y2: "))
+    x_val = int(input("Enter an x-value: "))
     coord1, coord2 = input_coordinates(x_coord1, x_coord2, y_coord1, y_coord2)
     y_value = y_coord_out(coord1, coord2, x_val)
-    print(y_value)
+    print("Corresponding y_value = {}".format(y_value))
 
 def input_coordinates(x_coord1, x_coord2, y_coord1, y_coord2):
     coord1 = (x_coord1, y_coord1)
@@ -19,13 +19,13 @@ def calc_slope(coord1, coord2):
     slope = (coord2[1] - coord1[1])/(coord2[0] - coord1[0])
     return slope
 
-def calc_intercept(coord1, coord2):
+def calc_intercept(coord1, coord2, slope):
     interc = coord1[1] - slope*coord1[0]
     return interc
 
 def y_coord_out(coord1, coord2, x_val):
     slope = calc_slope(coord1, coord2)
-    interc = calc_intercept(coord1, coord2)
+    interc = calc_intercept(coord1, coord2, slope)
     y_value = slope*x_val + interc
     return y_value
 
