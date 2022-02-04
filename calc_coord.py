d4d13031208d0d9950a0a0b17268033fe843d1c0
William Barbera <william.barbera@duke.edu>
2022-02-04 15:53:47 -0500

    Created function to calculate y-intercept of line.

--- calc_coord.py
+++ calc_coord.py
@@ -5,8 +5,7 @@ def calculate_value():
     y_coord1 = input("Y1: ")
     x_coord2 = input("X2: ")
     y_coord2 = input("Y2: ")
-    
-    coord1, coord2 = input_coordinates()
+    x_val = input("Enter an x-value: )
     slope = calc_slope(coord1, coord2)
     y_value = y_coord_out(slope, 5)
     print(y_value)
@@ -20,9 +19,12 @@ def calc_slope(coord1, coord2):
     slope = (coord2[1] - coord1[1])/(coord2[0] - coord1[0])
     return slope
 
+def calc_intercept(coord1, coord2):
+    interc = coord1[1] - slope*coord1[0]
+    return interc
 
-def y_coord_out(slope, x_val):
-    y_value = slope*x_val
+def y_coord_out(slope, interc, x_val):
+    y_value = slope*x_val + interc
     return y_value
 
 if __name__ == "__main__":
