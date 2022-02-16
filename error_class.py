4e2e6f05f9cc192a5956be5c79616f880d6fd044
William Barbera <william.barbera@duke.edu>
2022-02-16 15:39:15 -0500

    Error code from class.

---
+++ error_class.py
@@ -0,0 +1,36 @@
+"""
+a = "the sky is blue"
+print(a)
+
+for letter in a:
+    print(letter)
+   """ 
+
+def function_name():
+    try:
+        x = input("enter a number: ")
+        b = x - 3
+        print(b)
+    except TypeError
+    
+    
+    
+    """
+    try:
+        from my_calculator import sqrt
+    except ModuleNotFoundError: # python tries to run code in try
+    # if error is captured in except block, python runs this code
+        from math import sqrt
+        print("My_calculator module not available. Using default.")
+    
+    answer = sqrt(n)
+    return answer
+"""
+
+def main():
+    function_name()
+    #calc_square_root(4)
+
+
+if __name__ == "__main__":
+    main()
